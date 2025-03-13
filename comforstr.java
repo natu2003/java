1909d378822b4e7958053fb10530ac8777ea28ee
natu2003 <harinataraj24@gmail.com>
2025-03-13 11:48:51 +0530

    comparison for string

---
+++ comforstr.java
@@ -0,0 +1,8 @@
+public class comforstr{
+    public static void main(String[] args) {
+        String str = "goodmorning";
+        String str1 = "goodnight";
+        System.out.println(str == str1);
+        System.out.println(str != str1);
+    }
+}
