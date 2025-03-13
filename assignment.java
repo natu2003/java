5f99428dcde1b0c868c334f768843fe9cd88aa84
natu2003 <harinataraj24@gmail.com>
2025-03-13 11:35:30 +0530

    assignment

--- assignment.java
+++ assignment.java
@@ -1,7 +1,7 @@
 public class assignment{
     public static void main(String[] args) {
         int num =10;
-        int num2 =10;
+        int num2 =12;
         System.out.println(num>num2);
         System.out.println(num<num2);
         System.out.println(num>=num2); 
