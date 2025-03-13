7bbea2409e7f5e00d920cd91fcbc06b687a14bce
natu2003 <harinataraj24@gmail.com>
2025-03-13 10:17:06 +0530

    student detials

---
+++ std.java
@@ -0,0 +1,13 @@
+public class std{
+    public static void main(String[] args) {
+        String name = "natu";
+        String clas = "cse";
+        String percentage ="90";
+        int mark=680;
+        System.out.println("name: "+ name);
+        System.out.println("class: "+ clas);
+        System.out.println("percentage: "+ percentage);
+        System.out.println("mark: "+ mark);
+        
+    }
+}
