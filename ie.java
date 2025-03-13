3f65d4bf7ddeaa1658d8d98334d6b3417426a859
natu2003 <harinataraj24@gmail.com>
2025-03-13 15:25:25 +0530

    nested if

--- ie.java
+++ ie.java
@@ -1,11 +1,12 @@
 public class ie{
     public static void main(String[] args) {
-        int number = 10;
+        int number = 30;
 
         if (number > 0) {
-            System.out.println("This number is true");
-        } else if (number < 0) {
-            System.out.println("This number is false");
+            System.out.println("this condition is true");
+        } else if 
+        (number < 10) {
+            System.out.println("this condition is false");
                 }
     }
 }
