570bfd7db2a3870312c99d2bb869aedeb2c937b2
natu2003 <harinataraj24@gmail.com>
2025-03-13 12:04:25 +0530

    logical

---
+++ logical.java
@@ -0,0 +1,20 @@
+public class logical{
+    public static void main(String[] args) {
+        int a=10;
+        int b=30;
+        if(a>22 && b>18)
+        {
+            System.out.println("and condition true");
+        }
+        else{
+            System.out.println("and condition false");
+        }
+        if(a>22 || b>18)
+        {
+            System.out.println("or condition true");
+        }
+        else{
+            System.out.println("or condition false");
+        }
+    }
+}
