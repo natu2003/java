8a926452db113e2052f6a9a1d146f4b9a8e50d0d
natu2003 <harinataraj24@gmail.com>
2025-03-13 11:40:23 +0530

    comparison

---
+++ comparison.java
@@ -0,0 +1,12 @@
+public class comparison{
+    public static void main(String[] args) {
+        int num =10;
+        int num2 =12;
+        System.out.println(num>num2);
+        System.out.println(num<num2);
+        System.out.println(num>=num2); 
+        System.out.println(num<=num2);
+        System.out.println(num==num2);
+        System.out.println(num!=num2); 
+    }
+}
