dba909c1180768c4c482c70cdb8bd0c67f8618fd
natu2003 <harinataraj24@gmail.com>
2025-03-13 15:07:48 +0530

    ifelse

---
+++ ie.java
@@ -0,0 +1,13 @@
+public class ie{
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("This number is true");
+        } else if (number < 0) {
+            System.out.println("This number is false");
+                }
+    }
+}
+
+
