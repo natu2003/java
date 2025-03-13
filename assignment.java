79bdb9caee9ba874bd38740e5dc4108b5c816679
natu2003 <harinataraj24@gmail.com>
2025-03-13 11:28:23 +0530

    assignment

--- assignment.java
+++ assignment.java
@@ -2,11 +2,11 @@ public class assignment{
     public static void main(String[] args) {
         int num =10;
         int num2 =10;
-        System.out.println(num==num2);
-        System.out.println(num!=num2);
-        System.out.println(num>num2); 
+        System.out.println(num>num2);
         System.out.println(num<num2);
-        System.out.println(num>=num2);
-        System.out.println(num<=num2); 
+        System.out.println(num>=num2); 
+        System.out.println(num<=num2);
+        System.out.println(num==num2);
+        System.out.println(num!=num2); 
     }
 }
