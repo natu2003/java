b01955671bbcce73f5dbc133051ca30f2cc6373f
natu2003 <harinataraj24@gmail.com>
2025-03-13 10:49:12 +0530

    concat

---
+++ cons.java
@@ -0,0 +1,7 @@
+public class cons{
+    public static void main(String[] args) {
+        String myString = "Hello";
+        String myString2 = "world";
+        System.out.println(myString.concat(myString2));
+    }
+}
