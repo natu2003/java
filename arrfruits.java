18f07af7103a6362bda29daecfc7d1e1df3b169d
natu2003 <harinataraj24@gmail.com>
2025-03-14 15:19:41 +0530

    array in for

---
+++ arrfruits.java
@@ -0,0 +1,9 @@
+public class arrfruits{
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Cherry", "grapes", "orange"};
+        for (int i = 0; i < fruits.length; i++)
+         {
+        System.out.println("Fruit at index " + i + ": " + fruits[i]);
+         }
+    }
+}
