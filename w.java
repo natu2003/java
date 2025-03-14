7269315351f9d424a90199fb67af07d960260101
natu2003 <harinataraj24@gmail.com>
2025-03-14 11:28:01 +0530

    while loop

---
+++ w.java
@@ -0,0 +1,10 @@
+public class w{
+    public static void main(String[] args) {
+        int i=0;
+        String na="Nattu";
+        while(i<5)
+        {
+            System.out.println(na);
+        }
+    }
+}
