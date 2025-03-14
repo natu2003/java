c78663df2209c95b428297236b54afa5d132dce0
natu2003 <harinataraj24@gmail.com>
2025-03-14 13:44:45 +0530

    java method

---
+++ mymethod.java
@@ -0,0 +1,11 @@
+public class  mymethod{
+    static void mymethod()
+    {
+        System.out.println("multi:" +0*7);
+    }
+public static void main(String[] args) {
+    {
+        mymethod();
+    }
+}
+}
