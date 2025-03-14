09ed3f4eb3941651dd608b0e8dc84de51605d9f5
natu2003 <harinataraj24@gmail.com>
2025-03-14 13:51:58 +0530

    array

---
+++ ar.java
@@ -0,0 +1,7 @@
+public class ar{
+    public static void main(String[] args) {
+        int []a={1,2,3,4,5,6,7};
+        String []b={"apple","orange","grapes","banana"};
+        System.err.println(b[2]);
+    }
+}
