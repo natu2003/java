5ea16010940c76554c7937fe9511a339f9f9fdf5
natu2003 <harinataraj24@gmail.com>
2025-03-14 09:45:46 +0530

    days

--- day.java
+++ day.java
@@ -4,22 +4,22 @@ public class day{
 
         switch (day) {
             case 1:
-                System.out.println("Monday");
+                System.out.println("monday");
                 break;
             case 2:
-                System.out.println("Tuesday");
+                System.out.println("tuesday");
                 break;
             case 3:
-                System.out.println("Wednesday");
+                System.out.println("wednesday");
                 break;
             case 4:
-                System.out.println("Thursday");
+                System.out.println("thursday");
                 break;
             case 5:
-                System.out.println("Friday");
+                System.out.println("friday");
                 break;
             case 6:
-                System.out.println("Saturday");
+                System.out.println("saturday");
                 break;
             case 7:
                 System.out.println("Sunday");
