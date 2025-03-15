aef414b16aa7b4ba26050ef474e46b0753169965
natu2003 <harinataraj24@gmail.com>
2025-03-15 10:41:20 +0530

    customer bill

---
+++ storebill.java
@@ -0,0 +1,11 @@
+class storebill{
+    public static void main(String[] args) {
+    System.err.println("store billing system"); 
+    for (int i = 1; i < 5; i++) {
+     int invoinumber = 101;
+     invoinumber=invoinumber+i;
+     System.out.println("store billing for customer" + i +": "+ invoinumber);
+        
+    } 
+    }
+}
