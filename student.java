1ab105bdcccb65842483684da8799149a660af0a
natu2003 <harinataraj24@gmail.com>
2025-03-15 10:01:50 +0530

    student age

---
+++ student.java
@@ -0,0 +1,15 @@
+class age{
+    public void printage(){
+        int age = 22;
+        System.err.println("age is : " + age);
+    }
+}
+    
+public class student{
+
+public static void main(String[] args){
+    age obj = new age();
+    obj.printage();
+} 
+}
+
