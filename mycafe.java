1e39028588bb274223d14db006c757f2348a2105
natu2003 <harinataraj24@gmail.com>
2025-03-15 11:33:14 +0530

    tea

---
+++ mycafe.java
@@ -0,0 +1,28 @@
+public class mycafe{
+    public static void main(String[] args) {
+        int choice=1;
+        switch(choice){
+            case 1:
+            System.out.println("TEA");
+            break;
+            case 2:
+            System.out.println("COFFE");
+            break;
+            case 3:
+            System.out.println("VADAI");
+            break;
+            case 4:
+            System.out.println("IDLY");
+            break;
+            case 5:
+            System.out.println("POROTTA");
+            break;
+            default:
+                System.out.println("enjoy your snacks");
+        }
+
+
+
+        } 
+        
+    }
